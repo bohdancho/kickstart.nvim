5ed61bea51bee4df3175ab006e3c16a69fb42674
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-03 18:20:20 +0200

    remove unneeded comments in init.lua, fix treesitter by ignoring lua

--- init.lua
+++ init.lua
@@ -1,51 +1,6 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -323,69 +278,69 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
-  -- require('nvim-treesitter.configs').setup {
-  --   -- Add languages to be installed here that you want installed for treesitter
-  --   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
-  
-  --   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  --   auto_install = false,
-  
-  --   highlight = { enable = true },
-  --   indent = { enable = true },
-  --   incremental_selection = {
-  --     enable = true,
-  --     keymaps = {
-  --       init_selection = '<c-space>',
-  --       node_incremental = '<c-space>',
-  --       scope_incremental = '<c-s>',
-  --       node_decremental = '<M-space>',
-  --     },
-  --   },
-  --   textobjects = {
-  --     select = {
-  --       enable = true,
-  --       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-  --       keymaps = {
-  --         -- You can use the capture groups defined in textobjects.scm
-  --         ['aa'] = '@parameter.outer',
-  --         ['ia'] = '@parameter.inner',
-  --         ['af'] = '@function.outer',
-  --         ['if'] = '@function.inner',
-  --         ['ac'] = '@class.outer',
-  --         ['ic'] = '@class.inner',
-  --       },
-  --     },
-  --     move = {
-  --       enable = true,
-  --       set_jumps = true, -- whether to set jumps in the jumplist
-  --       goto_next_start = {
-  --         [']m'] = '@function.outer',
-  --         [']]'] = '@class.outer',
-  --       },
-  --       goto_next_end = {
-  --         [']M'] = '@function.outer',
-  --         [']['] = '@class.outer',
-  --       },
-  --       goto_previous_start = {
-  --         ['[m'] = '@function.outer',
-  --         ['[['] = '@class.outer',
-  --       },
-  --       goto_previous_end = {
-  --         ['[M'] = '@function.outer',
-  --         ['[]'] = '@class.outer',
-  --       },
-  --     },
-  --     swap = {
-  --       enable = true,
-  --       swap_next = {
-  --         ['<leader>a'] = '@parameter.inner',
-  --       },
-  --       swap_previous = {
-  --         ['<leader>A'] = '@parameter.inner',
-  --       },
-  --     },
-  --   },
-  -- }
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+
+    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+    auto_install = false,
+
+    highlight = { enable = true, disable = {'lua'} },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<M-space>',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
+  }
 end, 0)
 
 -- Diagnostic keymaps
