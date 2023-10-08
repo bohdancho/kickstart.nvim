7e3c21e2fc630dff9b924182cfa7d7a1af963e25
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-08 20:11:50 +0200

    make cmp work

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,11 +1,25 @@
 return {
   'hrsh7th/nvim-cmp',
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    'L3MON4D3/LuaSnip',
+    "saadparwaiz1/cmp_luasnip",
+  },
   opts = function()
     local cmp = require("cmp")
-    cmp.setup({
+
+    return {
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end
+      },
       mapping = cmp.mapping.preset.insert({
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete {},
@@ -15,8 +29,11 @@ return {
         }
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' }
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+        { name = 'luasnip' },
       })
-    })
+    }
   end
 }
